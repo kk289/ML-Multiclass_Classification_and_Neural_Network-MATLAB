44817c0c86b58f2f537d392db1dfbe6ceeda42b7
kk289 <kevilkhadka@gmail.com>
2020-06-16 01:08:18 -0700

    modify code

--- oneVsAll.m
+++ oneVsAll.m
@@ -22,12 +22,13 @@ X = [ones(m, 1) X];
 %               logistic regression classifiers with regularization
 %               parameter lambda. 
 
+% set initial theta
 initial_theta = zeros(n + 1, 1);
 
+% set options for fminunc
 options = optimset('GradObj', 'on', 'MaxIter', 50);
 
 for(i = 1:num_labels)
     all_theta(i,:) = fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), initial_theta, options);
-end;
-
+end
 end
